7d91fcc0e7d08aa0f58bc0ee99982bf2e88651d1
nhlong1512 <20521569@gm.uit.edu.vn>
2023-07-21 21:52:02 +0700

    Fetch SeatsList

--- ticket-booking-webapp/src/components/SeatsList.tsx
+++ ticket-booking-webapp/src/components/SeatsList.tsx
@@ -4,25 +4,116 @@ import Seat from './Seat'
 const SeatsList = () => {
   return (
     <div className='flex gap-[20px] flex-wrap'>
-        <Seat typeSeat='normal' statusSeat = 'none' />
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
-        <Seat typeSeat='normal' statusSeat = 'none'/>
+        <Seat typeSeat='vip' statusSeat = 'banned' seatId = {15} />
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'picked' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'picking' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='normal' statusSeat = 'none' seatId = {15}/>
+
+        <Seat typeSeat='vip' statusSeat = 'banned' seatId = {15} />
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'picked' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'picking' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='vip' statusSeat = 'none' seatId = {15}/>
+
+        <Seat typeSeat='sweet-box' statusSeat = 'banned' seatId = {15} />
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'picked' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'picking' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
+        <Seat typeSeat='sweet-box' statusSeat = 'none' seatId = {15}/>
     </div>
   )
 }
