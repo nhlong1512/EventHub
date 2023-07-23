be706fc5d896e7b5e5fa6cd1deef40e55929c82d
nhlong1512 <20521569@gm.uit.edu.vn>
2023-07-23 23:57:55 +0700

    Render all info seats picking in EventDescription and Confirmation Payment Modal

--- ticket-booking-webapp/src/components/EventDescription.tsx
+++ ticket-booking-webapp/src/components/EventDescription.tsx
@@ -7,16 +7,18 @@ import {
   DialogTitle,
   TextField,
 } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { ISeat } from "../models/ISeat";
-
 
 interface Props {
   seatsList: ISeat[];
   setSeatsList: (seatsList: ISeat[]) => void;
 }
 
+const PRICE_STANDARD = 100000;
+const PRICE_VIP = 200000;
+const PRICE_SWEET_BOX = 300000;
 
 const theme = createTheme({
   palette: {
@@ -26,24 +28,42 @@ const theme = createTheme({
   },
 });
 
-const EventDescription = ({seatsList, setSeatsList}:Props) => {
+const EventDescription = ({ seatsList, setSeatsList }: Props) => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [seatsPicking, setSeatsPicking] = useState<ISeat[]>([]); // [10, 23, 49
+  const [seatsNumber, setSeatsNumber] = useState<string>("");
   const handleClickPaymentBtn = () => {
     setOpen(true);
   };
-  
+
   const fetchPaymentPicking = () => {
     const seats = seatsList.filter((seat) => seat.statusSeat === "picking");
-    console.log(seats);
-  }
+    setSeatsNumber(seats.map((seat) => seat.seatId).join(", "));
+    setSeatsPicking(seats);
+  };
   useEffect(() => {
     fetchPaymentPicking();
-  }, [seatsList])
+  }, [seatsList]);
+
+  //Calculate total price
+  const totalTickets = seatsPicking.length;
+  const totalPrice = seatsPicking.reduce((total, seat) => {
+    switch (seat.typeSeat) {
+      case "standard":
+        return total + PRICE_STANDARD;
+      case "vip":
+        return total + PRICE_VIP;
+      case "sweet-box":
+        return total + PRICE_SWEET_BOX;
+      default:
+        return total;
+    }
+  }, 0);
 
   return (
-    <div className="mt-[80px] flex gap-[50px] p-[20px] rounded-[20px] border-[#ccc] border-solid border-[4px]">
+    <div className="mt-[80px] flex gap-[50px] p-[20px] rounded-[20px] border-[#ccc] border-solid border-[4px] min-h-[230px]">
       <div className="flex flex-row flex-[7] gap-[16px]">
         <div>
           <img
@@ -69,33 +89,50 @@ const EventDescription = ({seatsList, setSeatsList}:Props) => {
       </div>
       <div className="flex flex-[2] flex-col">
         <div className="flex flex-col gap-[16px]">
-          <div className="flex justify-between">
-            <p className="my-0">Standard (10)</p>
-            <p className="my-0">100.000 x 1</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="my-0">VIP (23)</p>
-            <p className="my-0">200.000 x 1</p>
-          </div>
-          <div className="flex justify-between">
-            <p className="my-0">SweetBox (49)</p>
-            <p className="my-0">300.000 x 1</p>
-          </div>
+          {seatsPicking.map((seat) => (
+            <div key={seat.seatId} className="flex justify-between">
+              <p className="my-0">
+                {seat.typeSeat === "standard"
+                  ? `Standard ${`(${seat.seatId})`}`
+                  : seat.typeSeat === "vip"
+                  ? `VIP ${`(${seat.seatId})`}`
+                  : `Sweet Box ${`(${seat.seatId})`}`}
+              </p>
+              <p className="my-0">
+                {seat.typeSeat === "standard"
+                  ? PRICE_STANDARD
+                  : seat.typeSeat === "vip"
+                  ? PRICE_VIP
+                  : PRICE_SWEET_BOX}{" "}
+                x 1
+              </p>
+            </div>
+          ))}
         </div>
-        <div className="flex justify-between my-[16px]">
-          <h3 className="my-0">Total</h3>
-          <h3 className="my-0">600.000 đ</h3>
-        </div>
-        <p className="my-0 mb-[16px]">(Number of tickets booked: 3/3)</p>
+        {totalTickets > 0 ? (
+          <div className="flex justify-between my-[16px]">
+            <h3 className="my-0">Total</h3>
+            <h3 className="my-0">{totalPrice} đ</h3>
+          </div>
+        ) : (
+          ""
+        )}
+        <p className="my-0 mb-[16px]">
+          (Number of tickets booked: {totalTickets}/3)
+        </p>
         <ThemeProvider theme={theme}>
-          <Button
-            variant="contained"
-            component="label"
-            className="rounded-[10px]"
-            onClick={handleClickPaymentBtn}
-          >
-            Payment
-          </Button>
+          {totalTickets > 0 ? (
+            <Button
+              variant="contained"
+              component="label"
+              className="rounded-[10px]"
+              onClick={handleClickPaymentBtn}
+            >
+              Payment
+            </Button>
+          ) : (
+            ""
+          )}
           <Dialog open={open} onClose={handleClose}>
             <DialogTitle
               className="text-main"
@@ -223,7 +260,7 @@ const EventDescription = ({seatsList, setSeatsList}:Props) => {
                   id="text"
                   label="Seats"
                   type="text"
-                  defaultValue="10, 23, 49"
+                  defaultValue={seatsNumber}
                   InputProps={{
                     readOnly: true,
                   }}
