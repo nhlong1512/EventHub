39a1bb6a0a3dc2f070c7bc44a9fdf845bc26710d
nhlong1512 <20521569@gm.uit.edu.vn>
2023-07-23 18:22:32 +0700

    logic seat click

--- ticket-booking-webapp/src/components/Seat.tsx
+++ ticket-booking-webapp/src/components/Seat.tsx
@@ -1,26 +1,47 @@
 import React from "react";
 import classNames from "classnames";
+import { ISeat } from "../models/ISeat";
 
 interface Props {
   typeSeat: string;
   statusSeat: string;
   seatId: number;
+  pickingSeats: ISeat[];
+  setPickingSeats: (pickingSeats: ISeat[]) => void;
+  seatsList: ISeat[];
+  // onClick: (seatId: number) => void;
 }
 
-const Seat = ({ typeSeat, statusSeat, seatId }: Props) => {
+const Seat = ({
+  typeSeat,
+  statusSeat,
+  seatId,
+  pickingSeats,
+  setPickingSeats,
+  seatsList,
+}: Props) => {
+  const handleClickSeat = () => {
+    // onClick(seatId);
+    const seat = seatsList.find((seat) => seat.seatId === seatId)
+    if (seat) {
+      console.log(seat);
+    }
+  };
   return (
     <div
       className={classNames(
         "w-[40px] h-[40px] border-[2px] border-solid rouned-[2px] flex justify-center items-center cursor-pointer hover:opacity-80",
         {
-          "bg-[#ccc] border-[#ccc] text-[#fff]": statusSeat === "banned" || statusSeat === "picked",
-          "bg-[#88050c] border-[#88050c] text-[#fff]": statusSeat ==="picking",
-          "border-main": statusSeat ==="default" && typeSeat ==="standard",
-          "border-[#e50914]": statusSeat ==="default" && typeSeat ==="vip",
-          "border-[#ee7a7a] bg-[#ee7a7a] text-[#fff]": statusSeat ==="default" && typeSeat ==="sweet-box",
-
+          "bg-[#ccc] border-[#ccc] text-[#fff]":
+            statusSeat === "banned" || statusSeat === "picked",
+          "bg-[#88050c] border-[#88050c] text-[#fff]": statusSeat === "picking",
+          "border-main": statusSeat === "default" && typeSeat === "standard",
+          "border-[#e50914]": statusSeat === "default" && typeSeat === "vip",
+          "border-[#ee7a7a] bg-[#ee7a7a] text-[#fff]":
+            statusSeat === "default" && typeSeat === "sweet-box",
         }
       )}
+      onClick={handleClickSeat}
     >
       <p
         className={classNames("m-0", {
